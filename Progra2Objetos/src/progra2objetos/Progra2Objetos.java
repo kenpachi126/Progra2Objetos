b4bb1fb452b75c2235647d457c8b46872fae7995
kenpachi126 <kevinandres.126@gmail.com>
2015-05-25 20:23:26 -0600

    inicio

---
+++ Progra2Objetos/src/progra2objetos/Progra2Objetos.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2objetos;
+
+/**
+ *
+ * @author kevin
+ */
+public class Progra2Objetos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
